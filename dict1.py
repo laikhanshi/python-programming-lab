fbb3b54246ff9a8511c9379a39c74b05039c63fd
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:20:27 +0530

    Create dict1.py

---
+++ dict1.py
@@ -0,0 +1,10 @@
+# Python program to create an empty dictionary
+
+# creating an empty dictionary
+dict_a = dict()
+
+# printing the dictionary
+print("dict_a :", dict_a)
+
+# printing the length
+print("Total elements: ", len(dict_a))
