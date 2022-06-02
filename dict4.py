ede711d905fd82531ec2a6ee785a349808edd6c1
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:23:20 +0530

    Create dict4.py

---
+++ dict4.py
@@ -0,0 +1,11 @@
+# Python program to access elements 
+# from a Dictionary using key name
+
+# Dictionary 
+Record = {'id' : 101, 'name' : 'Amit Kumar', 'age' : 21} 
+
+# accessing the elements
+print("Dictionary \'Record\' elements...")
+print(Record['id'])
+print(Record['name'])
+print(Record['age'])
