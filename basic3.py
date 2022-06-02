c4d81013904c1e18124bcc1ec0182b73d3a8de92
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:18:12 +0530

    Create basic3.py

---
+++ basic3.py
@@ -0,0 +1,16 @@
+# Python code to print text/string using 
+# user-defined method
+
+# function defintion of "putMe()"
+# it will accepts a string and print it on the screen
+def putMe(text):
+    # printing text on the screen
+    print(text)
+
+# main code
+if __name__ == '__main__':
+    putMe('Hello world!')
+    putMe('Welcome @')
+    putMe('IncludeHelp')
+    putMe('The world of programming')
+    putMe('A programming community for developer and students')
