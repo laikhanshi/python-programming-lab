8893ed19105b58de2b21d91df1eabfa18293d7d2
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:22:37 +0530

    Create dict3.py

---
+++ dict3.py
@@ -0,0 +1,12 @@
+# Python program to 
+# create a dictionary from a sequence
+
+# creating dictionary
+dic_a = dict([(1,'apple'), (2,'ball'), (3,'cat')])
+
+# printing the dictionary
+print("dict_a :", dic_a)
+
+# printing key-value pairs
+for x,y in dic_a.items():
+    print(x,':',y)
