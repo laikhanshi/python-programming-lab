1ed29996833d04d081ec63c6c3a31dc8491d1e3e
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:19:44 +0530

    Create basic5.py

---
+++ basic5.py
@@ -0,0 +1,9 @@
+# input two numbers: value of a and b
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+# find sum of a and b and assign to c
+c = a+b
+
+# print sum (c)
+print("Sum: ",c)
