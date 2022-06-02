a75f5b97296f70afe50b3d49ed19ee57a3fecd28
laikhanshi <106541827+laikhanshi@users.noreply.github.com>
2022-06-02 22:52:07 +0530

    Create random3.py

---
+++ random3.py
@@ -0,0 +1,12 @@
+Write a Python program to set a random seed and get a random number between 0 and 1.
+import random 
+print("Set a random seed and get a random number between 0 and 1:")
+random.seed(0)
+new_random_value = random.random()
+print(new_random_value)
+random.seed(1)
+new_random_value = random.random()
+print(new_random_value)
+random.seed(2)
+new_random_value = random.random()
+print(new_random_value)
